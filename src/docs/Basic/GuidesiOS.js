5ca719f0dc37d842fbd438754033db15ed2db301
kha-github <rlahyuna1234@gmail.com>
2021-05-27 00:38:06 +0900

    update guidesiOS

--- src/docs/Basic/GuidesiOS.js
+++ src/docs/Basic/GuidesiOS.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, Image, Dimensions, View, ScrollView } from 'react-native';
+import { OuterView, SrcTouchable } from '~/styles/innerpageStyle';
+import { TextMarkdown, CodeMarkdown } from '~/components/Markdown/MarkdownBlock';
+import * as content from "./GuidesiOSContent";
 
 export default GuidesiOS = (props) => {
 	return (
-		<ScrollView style={{ flex: 1 }}>
+		<ScrollView
+			contentInsetAdjustmentBehavior="automatic">
+			<OuterView>
+				<TextMarkdown source={content.text1_1}/>
+				<TextMarkdown source={content.text2_1}/>
+				<TextMarkdown source={content.text3_1}/>
+				<TextMarkdown source={content.text4_1}/>
+				<TextMarkdown source={content.text5_1}/>
+			</OuterView>
 		</ScrollView>
 	)
 }
